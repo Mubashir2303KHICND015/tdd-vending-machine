dd2f5d6dc19ea1ab37d97d5487a35a9a03d88e87
Mubashir2303KHICND015 <mubashir.alam@xloopdigital.com>
2023-04-12 20:53:41 +0500

    Test 2 Passed!

--- src/Machine.js
+++ src/Machine.js
@@ -1,8 +1,17 @@
 module.exports = class Machine {
     constructor() {
-
+        this.depositedMoney = 0
+        this.billsAccepted = [10,20,30,40,50,100] 
     }
 
     selections = [{'crisps': 100}, {'chocolate': 350}, {'mints': 70}]
     seeSelections = ()=>this.selections
+    deposit = (money)=>{
+        if(this.billsAccepted.includes(money)){
+            this.depositedMoney += money
+            return `You have deposited Rs ${this.depositedMoney}`
+        }else{
+            return "Please input bills like 10,20,30 etc"
+        }
+    }
 };
